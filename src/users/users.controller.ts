de73b4c259af57bba27c2993d1b9112c01d7497c
Roman Litvinov <litvinovroman911@gmail.com>
2022-09-28 10:30:46 +0100

    Bugfix/Users (#6)
    Added Auth guards to Users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, Param, UseGuards } from '@nestjs/common'
 import { UserDto } from './dto/user.dto'
 import { UsersService } from './users.service'
+import { AuthJwtGuard } from '../auth/guards/auth-jwt.guard'
 
 @Controller('users')
+@UseGuards(AuthJwtGuard)
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
